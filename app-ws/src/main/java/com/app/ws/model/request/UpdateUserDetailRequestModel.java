81e56ce4ac9d29e77e4723457e3b9dcb0c6e8111
saivamsitammina <81849222+wasif22001@users.noreply.github.com>
2023-12-31 23:58:59 +0500

    Created update endpoint.

--- app-ws/src/main/java/com/app/ws/model/request/UpdateUserDetailRequestModel.java
+++ app-ws/src/main/java/com/app/ws/model/request/UpdateUserDetailRequestModel.java
@@ -5,7 +5,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -13,17 +12,11 @@ import javax.validation.constraints.Size;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserDetailRequestModel {
+public class UpdateUserDetailRequestModel {
     @NotNull(message = "First name cannot be null")
     @Size(min = 2, message = "first name must not be less than 2 characters")
     private String firstName;
     @NotNull(message = "last name cannot be null")
     @Size(min = 2, message = "last name must not be less than 2 characters")
     private String lastName;
-    @NotNull(message = "email cannot be null")
-    @Email
-    private String email;
-    @NotNull(message = "user id cannot be null")
-    @Size(min = 8, max = 16, message = "password must be equal or greater than 8 characters and less than or equal to 16 characters")
-    private String password;
 }
